3379c54cd9848cbc216271d4f4f213a34c96287b
DESKTOP-E4KDVER\user <tbmatome@gmail.com>
2026-09-16 14:54:17 +0200

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://matomesofttech.co.za',
 	vite: {
 		plugins: [tailwindcss()],
 	},
